a42c9d437b127d642a52006d2764b8f6c6f8d0e7
Francesco Bertani <franz.bertani@gmail.com>
2017-11-12 18:27:10 +0100

    comments on example

--- C#/LedBarManager/Program.cs
+++ C#/LedBarManager/Program.cs
@@ -1,30 +1,46 @@
-﻿using System;
+﻿/// Just an example...
+
+using System;
 
 namespace LedBarManager
 {
     public class Program
     {
+
         static void Main()
         {
+            /// Create LedBarManager object.
+            /// Port name is set based on my Mac, probabily you should change it.
+            LedBarManager lb = new LedBarManager("/dev/cu.usbmodem1411");
 
-            LedBarManager gp = new LedBarManager("/dev/cu.usbmodem1411");
-            gp.SetTeamColor(1, "FF00FF");
-            gp.SetTeamColor(2, "453400");
-            gp.StartProgressBar(10);
+            /// Set the color for the teams
+            lb.SetTeamColor(1, "FF00FF");
+            lb.SetTeamColor(2, "453400");
 
-            gp.AddPoint(1, 4);
-            gp.AddPoint(2, 3);
-            gp.AddPoint(1, 1);
+            /// Ready, set, go. 
+            /// 10 is the total number of points to win the game.
+            lb.StartProgressBar(10);
+
+            /// Add some points here and there.
+            /// Not possible to overflow: if 10 is the maximum number of points
+            /// each team can go up to 10.
+            /// lb.AddPoint(teamNumber) will add 1 point by default.
+            lb.AddPoint(1, 4);
+            lb.AddPoint(2, 3);
+            lb.AddPoint(1, 1);
+
+            /// Just to let things go a little bit slower...
+            System.Threading.Thread.Sleep(1000);
+            lb.AddPoint(1);
+            lb.AddPoint(2, 5);
 
             System.Threading.Thread.Sleep(1000);
-            gp.AddPoint(1);
-            gp.AddPoint(2, 5);
+            lb.AddPoint(1, 10);
 
+            /// Not really working now... 
+            /// Need to fix on Arduino side...
             System.Threading.Thread.Sleep(1000);
-            gp.AddPoint(1, 10);
-
-            System.Threading.Thread.Sleep(1000);
-            gp.ShowEffect(0, 3, 2000);
+            lb.ShowEffect(0, 3, 2000);
 
 
 
